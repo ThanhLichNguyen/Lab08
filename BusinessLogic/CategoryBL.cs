4e153705a2fb679a50caab43b9b3543b3ef84eb9
mnlac <nthanhlich10@gmail.com>
2021-11-30 23:04:56 +0700

    first commit

---
+++ BusinessLogic/CategoryBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class CategoryBL
+    {
+        CategoryDA categoryDA = new CategoryDA();
+        public List<Category> GetAll()
+        {
+            return categoryDA.GetAll();
+        }
+        public int Insert(Category category)
+        {
+            return categoryDA.Insert_Update_Delete(category, 0);
+        }
+        //Phương thức cập nhật dữ liệu
+        public int Update(Category category)
+        {
+            return categoryDA.Insert_Update_Delete(category, 1);
+        }
+        //Phương thức xoá dữ liệu truyền vào ID
+        public int Delete(Category category)
+        {
+            return categoryDA.Insert_Update_Delete(category, 2);
+        }
+    }
+}
